c1e01aab545142f7da3ef2b85d2e741e9b6c220d
Simon <simon.kohl@student.kit.edu>
2014-11-27 21:32:47 +0100

    Code leserlicher

--- readIn.py
+++ readIn.py
@@ -12,25 +12,17 @@ column2bfilled2 = pd.DataFrame(col_dat,columns={5})
 
 df = pd.concat([df,column2bfilled1,column2bfilled2],axis=1)
 
-print df
-
 def convert_date(d):
     d=str(d)
     year=d[:-4]
     month=d[2:4]
     day=d[4:6]
-    #return b+'/'+c+'/'+'20'+a
     return '20'+year+'-'+month+'-'+day
 
 for row in range(n_data):
-  print row
   
   #!!!cutting off 6 letters is only necessary in old data!!!
   date_clean = df[0].as_matrix()[row][:-6]
-
-  df[0][row] = date_clean
-  print date_clean
-
   dep = df[1].as_matrix()[row]
   ret = df[2].as_matrix()[row]
 
@@ -40,15 +32,15 @@ for row in range(n_data):
   departure = np.datetime64(dep_date)
   return_ = np.datetime64(ret_date)
   query = np.datetime64(date_clean)
-  
-  df[1][row] = dep_date
-  df[2][row] = ret_date
-  
+
   delta1 = return_-departure
   delta2 = departure-query
   delta1_r = round(delta1/np.timedelta64(1, 'm'))
   delta2_r = round(delta2/np.timedelta64(1, 'm'))
   
+  df[0][row] = date_clean
+  df[1][row] = dep_date
+  df[2][row] = ret_date
   df[4][row] = delta1_r
   df[5][row] = delta2_r
  
